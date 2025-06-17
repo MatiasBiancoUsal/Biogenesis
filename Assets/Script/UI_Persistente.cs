8a5f8cc0736b3a806ce75fb8df722607f43d62a4
LucyRosehearts <luciana.cardosobocca@usal.edu.ar>
2025-06-17 12:48:30 -0300

    boton

--- Assets/Script/UI_Persistente.cs
+++ Assets/Script/UI_Persistente.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class UIPersistente : MonoBehaviour
 {
+    public static UIPersistente Instance;
     void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
         DontDestroyOnLoad(gameObject);
+        
     }
 }
