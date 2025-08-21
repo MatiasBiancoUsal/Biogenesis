f4ca163113495af22355dee598ed935075c3b2dd
LucyRosehearts <luciana.cardosobocca@usal.edu.ar>
2025-08-20 22:51:05 -0300

    animaciones

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -5,8 +5,19 @@ public class Personaje : MonoBehaviour
     public int vida = 100;
     public int vidaMaxima = 100;
 
+    // Script lucy
+    private Animator animator;
+    private bool estaMuerto = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // buscamos el Animator en el personaje
+    }
+//
     public void TomarDaño(int cantidad)
     {
+        if (estaMuerto) return; // si ya murió, no recibe más daño
+
         vida -= cantidad;
         Debug.Log("Personaje recibió daño. Vida actual: " + vida);
         if (vida <= 0)
@@ -30,6 +41,14 @@ public class Personaje : MonoBehaviour
             Debug.Log("Personaje se curó. Vida actual: " + vida);
         }
     }
+
+    private void Morir()
+    {
+        estaMuerto = true;
+        Debug.Log("Personaje ha muerto");
+        animator.SetTrigger("muerte"); // Dispara la animación de muerte
+    }
+
 }
 
 
