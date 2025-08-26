e8e4d038f0b2a1b414833933b334f54d4e5cd350
soo2003 <alvarez.s2@usal.edu.ar>
2025-08-25 21:41:01 -0300

    Hice que cada comida sea espesifica para cada criartura y que se pueda arrastrar fuera de la bandeja

---
+++ Assets/Script/Bandeja comida criaturas/CriaturaComidaEsp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriaturaComidaEsp : MonoBehaviour
+{
+    [Header("La comida que esta criatura acepta")]
+    public GameObject comidaAceptada; // arrastrá acá el prefab 2D correcto
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Bandeja comida criaturas/DragItemComidaUI.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragItemComidaUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("Prefab 2D correspondiente a este icono UI")]
+    public GameObject comida2DPrefab;
+
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+
+    private Vector3 startPos;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startPos = rectTransform.position;
+
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f; // transparencia mientras arrastro
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.position += (Vector3)eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Posición del mouse en el mundo
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mouseWorldPos2D = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+
+        // Raycast hacia la escena 2D
+        RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos2D, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            CriaturaComidaEsp criatura = hit.collider.GetComponent<CriaturaComidaEsp>();
+            if (criatura != null)
+            {
+                // Validamos si esta criatura acepta este tipo de comida
+                if (criatura.comidaAceptada == comida2DPrefab)
+                {
+                    // Instancia rápida del prefab (opcional: sirve para animaciones)
+                    GameObject comida = Instantiate(comida2DPrefab, mouseWorldPos2D, Quaternion.identity);
+
+                    // Destruir al instante (no queda en escena)
+                    Destroy(comida);
+
+                    // Eliminar el icono de la bandeja (consumir comida)
+                    Destroy(gameObject);
+
+                    Debug.Log(criatura.name + " comió su comida!");
+                }
+                else
+                {
+                    Debug.Log(criatura.name + " no acepta esta comida.");
+                    rectTransform.position = startPos; // vuelve a la bandeja
+                }
+            }
+            else
+            {
+                rectTransform.position = startPos;
+            }
+        }
+        else
+        {
+            rectTransform.position = startPos;
+        }
+
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+    }
+}
