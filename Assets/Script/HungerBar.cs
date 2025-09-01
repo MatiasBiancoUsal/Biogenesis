87c01caab06f14cc1fdf749a85200cef7897ce65
AgostinaMonzon <monzonagostina67@gmail.com>
2025-09-01 17:01:54 -0300

    Guardado de partida y id

--- Assets/Script/HungerBar.cs
+++ Assets/Script/HungerBar.cs
@@ -6,12 +6,25 @@ public class HungerBar : MonoBehaviour
     public Image hungerFill; // Asigna aquí la Image de la barra
     public float hungerValue = 1f; // Entre 0 y 1
     public float hungerDecreaseRate = 0.01f; // Cuánto baja por segundo
+    [Header("Identificador único de la criatura")]
+    public string creatureID = "Alimaña";
+    //  Cambiás este valor en el Inspector: "Alimaña", "Mutante", "Araña" o "Experimento"
 
+    private void Start()
+    {
+        // Cargar hambre guardada
+        hungerValue = PlayerPrefs.GetFloat("Hambre_" + creatureID, 1f);
+        UpdateHungerUI();
+    }
     void Update()
     {
         hungerValue -= hungerDecreaseRate * Time.deltaTime;
         hungerValue = Mathf.Clamp01(hungerValue);
         UpdateHungerUI();
+
+        //Guardar automáticamente
+        PlayerPrefs.SetFloat("Hambre_" + creatureID, hungerValue);
+        PlayerPrefs.Save();
     }
 
     public void Feed(float amount)
@@ -19,6 +32,9 @@ public class HungerBar : MonoBehaviour
         hungerValue += amount;
         //hungerValue = Mathf.Clamp01(hungerValue);
         UpdateHungerUI();
+
+        PlayerPrefs.SetFloat("Hambre_" + creatureID, hungerValue);
+        PlayerPrefs.Save();
     }
 
     void UpdateHungerUI()
