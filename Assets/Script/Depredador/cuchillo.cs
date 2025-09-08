5096d6157a01ac29a851449024802c7f54c1c2c9
soo2003 <alvarez.s2@usal.edu.ar>
2025-09-07 23:50:32 -0300

    .

--- Assets/Script/Depredador/cuchillo.cs
+++ Assets/Script/Depredador/cuchillo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class cuchillo : MonoBehaviour
 {
     
-    public int daño = 1;
+    public int daño = 10;
 
     // Start is called before the first frame update
     void Start()
