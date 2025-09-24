9f66c9b910db2d81b6dcdb3570e651e47bd6d692
soo2003 <alvarez.s2@usal.edu.ar>
2025-09-24 01:19:49 -0300

    arreglo puerta laboratorio menú pausa

--- Assets/Script/Pantalla estado de criaturas/EscenaPantalla.cs
+++ Assets/Script/Pantalla estado de criaturas/EscenaPantalla.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class EscenaPantalla : MonoBehaviour
@@ -21,6 +22,9 @@ public class EscenaPantalla : MonoBehaviour
 
    void OnMouseDown()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
         SceneManager.LoadScene(IrAEscena);
     }
 }
